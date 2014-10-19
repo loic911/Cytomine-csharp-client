a1b0b222e4b5f9a4bafe6cbcd74b7923f3b04231
Loïc Rollus <loicrollus@gmail.com>
2014-10-19 14:28:25 +0200

    remove comment

--- Cytomine.cs
+++ Cytomine.cs
@@ -86,22 +86,7 @@ public class Cytomine
 
 	public static string CreateSignature (string publicKey, string privateKey, string messageToSign)
 	{
-			/**publicKey=89279a28-9907-4014-9cb8-dde6682f1435
-			privateKey=7af025b6-0687-4909-b4d2-f5df53777532
-			messageToSign=GET
-
-
-			Fri, 26 Sep 2014 08:19:32 +0000
-			/api/storage/17763541.json
-			signature=TjwiXi9xGyr34NjA6TzsBLBAPrg=
-			authorization=CYTOMINE 89279a28-9907-4014-9cb8-dde6682f1435:TjwiXi9xGyr34NjA6TzsBLBAPrg=
-			**/
-
-
-			//string publicKey="89279a28-9907-4014-9cb8-dde6682f1435";
-			//string privateKey="7af025b6-0687-4909-b4d2-f5df53777532";
-			//string messageToSign = "GET\n\n\nFri, 26 Sep 2014 08:19:32 +0000\n/api/storage/17763541.json";
-
+	
 		HMACSHA1 hmacsha1 = new HMACSHA1();
 		string secretKey = privateKey;
 		string content = messageToSign;
@@ -112,8 +97,6 @@ public class Cytomine
 		hmacsha1.Key = secretKeyBArr;
 		byte[] final = hmacsha1.ComputeHash(contentBArr);
 		string signature = System.Convert.ToBase64String(final);
-		//Console.WriteLine (signature + " ======== " +"TjwiXi9xGyr34NjA6TzsBLBAPrg=");
-	
 		String authorization = "CYTOMINE " + publicKey + ":" + signature;
 
 		return authorization;
